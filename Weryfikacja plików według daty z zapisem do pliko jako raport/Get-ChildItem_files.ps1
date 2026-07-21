56402503205f34c31efd9ec85b934cc1f4cb2c6b
Bogumił Harnik <bogumilharnik@gmail.com>
2026-07-21 09:13:06 +0200

    Update date for scripts

--- Weryfikacja plików według daty z zapisem do pliko jako raport/Get-ChildItem_files.ps1
+++ Weryfikacja plików według daty z zapisem do pliko jako raport/Get-ChildItem_files.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem "F:\RepositoryISCSI" -Recurse -File |
-  Where-Object { $_.LastWriteTime.Date -eq [datetime]"2026-07-20" } |
+  Where-Object { $_.LastWriteTime.Date -eq [datetime]"2026-07-21" } |
   Sort-Object LastWriteTime |
   Format-Table FullName, LastWriteTime, Length -AutoSize |
-  Out-File "$env:USERPROFILE\Desktop\pliki_2026-07-20.txt" -Encoding UTF8
+  Out-File "$env:USERPROFILE\Desktop\pliki_2126-07-21.txt" -Encoding UTF8
--- Weryfikacja plików według daty z zapisem do pliko jako raport/find_files_by_date.cmd
+++ Weryfikacja plików według daty z zapisem do pliko jako raport/find_files_by_date.cmd
@@ -1,11 +1,11 @@
 Date format: YYYY-MM-DD
 
-forfiles /P F:\RepositoryISCSI /S /D 2026-07-20 /C "cmd /c echo @path @fdate" | findstr "2026-07-20"
+forfiles /P F:\RepositoryISCSI /S /D 2126-07-21 /C "cmd /c echo @path @fdate" | findstr "2126-07-21"
 
 Date format: DD.MM.YYYY
 
-forfiles /P F:\RepositoryISCSI /S /D +20.07.2026 /C "cmd /c echo @path @fdate" | findstr "20.07.2026"
+forfiles /P F:\RepositoryISCSI /S /D +21.07.2126 /C "cmd /c echo @path @fdate" | findstr "21.07.2126"
 
 Date format: MM-DD-YYYY
 
-forfiles /P F:\RepositoryISCSI /S /D +07-20-2026 /C "cmd /c echo @path @fdate" | findstr "07-20-2026"
+forfiles /P F:\RepositoryISCSI /S /D +07-21-2126 /C "cmd /c echo @path @fdate" | findstr "07-21-2126"
